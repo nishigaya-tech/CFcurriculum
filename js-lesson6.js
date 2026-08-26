e664503d50ae27588bbfe1d5b183d5ea1b4553ea
nishigaya-tech <nishigaya@center-f.com>
2026-08-26 18:50:58 +0900

    Add files via upload

---
+++ js-lesson6.js
@@ -0,0 +1,16 @@
+let Lastname = '金 為子';
+console.log('私は、'+Lastname);
+
+//結婚して苗字が変わったと想定して
+
+Lastname = '五味 為子';
+console.log('私は、'+Lastname);
+
+//上記のコードの書き方をまねして、実際に変数を宣言 → 出力 → 再代入 → 出力してみましょう！
+//この下にコードを書いてね！
+
+let food = '猫';
+console.log('私は'+food+'が好きです');
+
+food = '犬';
+console.log('私は'+food+'も好きです');
