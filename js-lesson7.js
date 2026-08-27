eb24e23bacd2a5b91f587805b2c7c7857c8f5c32
nishigaya-tech <nishigaya@center-f.com>
2026-08-27 19:01:03 +0900

    Add files via upload

---
+++ js-lesson7.js
@@ -0,0 +1,26 @@
+
+let year = 2023;
+
+// 以下は全部、変数yearに1を足して再代入する処理！
+//実際に出力をして確認するとより理解が深まるよ！
+
+year += 1;
+console.log(year);
+
+year++;
+console.log(year);
+
+// 足す以外にも「割る」「引く」「掛ける」「余りを出す！」事ができるよ！
+//実際に出力をして確認するとより理解が深まるよ！
+
+year /= 1 //yearを１で割ってyearに代入
+console.log(year);
+
+year -= 1 ;//yearから1を引いてyearに代入
+console.log(year);
+
+year *= 1; //yearに１をかけるてyearに代入
+console.log(year);
+
+year %= 10 //yearを10で割って、その余りをyearに代入
+console.log(year);
