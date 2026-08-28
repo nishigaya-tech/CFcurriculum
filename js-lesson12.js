2140f77e9affc02d20c9aaab8706184c481514fb
nishigaya-tech <nishigaya@center-f.com>
2026-08-28 18:56:49 +0900

    Add files via upload

---
+++ js-lesson12.js
@@ -0,0 +1,18 @@
+//好きに値を変えて試してみてね！！
+
+const pokemon ='ポッチャマ';
+
+switch(pokemon){
+    case 'ポッチャマ':
+        console.log('ぽちゃぽちゃ');
+        break;
+    case 'ニャース':
+        console.log('ニャースでニャース');
+        break
+    case 'ピカチュウ':
+        console.log('ピッ、ピカチュー！');
+        break
+    default:
+        console.log('ただの、変なおっさんだ！');
+        break
+}
